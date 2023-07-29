67fa891906ccbd234df0332981cd0c8f19947692
shubham <gshubham934@gmail.com>
2023-07-29 23:36:50 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <>
+     <div className="App">
+        Launching Soon !!
+        
+    </div>
+    <img src="/images/logo192.png"alt="logo" />
+    
+    </>
+   
+  );
+}
+
+export default App;
